cd48f05e70b3906a90930bed2b42582f6291886c
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-07-11 16:14:02 +0300

    feat: added counter

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,7 +1,72 @@
 import React, { Component } from 'react';
 
-export class Counter extends Component {
+interface Props {}
+
+interface State {
+  count: number; 
+}
+
+export class Counter extends Component <Props, State>{
+  constructor(props: Props | Readonly<Props>) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    console.log('constructor');
+  }
+
+  static getDerivedStateFromProps(props: Props, state: State) {
+    console.log('getDerivedStateFromProps');
+    return null;
+  }
+
+  componentDidMount() {
+    alert(`componentDidMount ${this.state.count}`);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate(_prevProps: Props, prevState: State) {
+    alert(`componentDidUpdate, ${this.state.count}`);
+    alert(`componentDidUpdate prev: ${prevState.count}`);
+  }
+
+  increment(): void {
+    this.setState(({count}) => ({
+      count: count + 1
+    }));
+  }
+
+  decrement(): void {
+    this.setState(({count}) => ({
+      count: count - 1
+    }));
+  }
+
   render() {
-    return <h1>Components works</h1>
+    if(this.state.count === 5) {
+      throw new Error(`I'm crashed`)
+    }
+    return (
+      <>
+        <button onClick={this.decrement}>minus</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>plus</button>
+      </>
+    );
   }
 }
